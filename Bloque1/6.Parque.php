03b9151e38c28f5923bfea56adca453de46f0387
Adrian Gomez <agomezdo18dw@ikzubirimanteo.com>
2019-09-17 19:51:50 +0200

    Finalizado ejercicio6

--- Bloque1/6.Parque.php
+++ Bloque1/6.Parque.php
@@ -5,12 +5,27 @@
 </head>
 <body>
     <p>Queremos controlar la entrada a una atracción del parque. Se
-pueden subir los mayores de 10 años o los que midan más de 120
-cm. Si el menor está ‘acompañado’ se le permite subir a la
-atracción si la edad es mayor de 6 años, no importa la altura.</p>
+    pueden subir los mayores de 10 años o los que midan más de 120
+    cm. Si el menor está ‘acompañado’ se le permite subir a la
+    atracción si la edad es mayor de 6 años, no importa la altura.</p>
 	<?php
     $edad = rand(1,100);
     $altura = rand(100,200);
+    $acompañado = rand(0,1);
+    if ($acompañado == 1)
+        $acompañado = "Acompañado";
+    else
+        $acompañado = "No acompañado";
+    echo "<p><b>La edad de la persona es:</b> $edad <br>
+    <b>La altura de la persona es:</b> $altura <br>
+    <b>La persona esta acompañada:</b> $acompañado</p>";
+
+    if ($edad >= 10 or $altura >= 120)
+        echo "Puede subir";
+    elseif ($edad >= 6 and $acompañado == "Acompañado")
+        echo "Puede subir";
+    else
+        echo "No puede subir";
 	?>
 </body>
 </html>
