e0a2b64c20bc173ce1304967f5ba22284076111d
Adrian Gomez <agomezdo18dw@ikzubirimanteo.com>
2019-09-17 16:59:01 +0200

    Comienzo ejercicio 6

---
+++ Bloque1/6.Parque.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio PHP/6.Parque</title>
+</head>
+<body>
+    <p>Queremos controlar la entrada a una atracción del parque. Se
+pueden subir los mayores de 10 años o los que midan más de 120
+cm. Si el menor está ‘acompañado’ se le permite subir a la
+atracción si la edad es mayor de 6 años, no importa la altura.</p>
+	<?php
+    $edad = rand(1,100);
+    $altura = rand(100,200);
+	?>
+</body>
+</html>
