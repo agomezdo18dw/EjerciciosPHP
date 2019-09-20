e49d10f03503f0665728bd784e5b4482c41a4196
Adrian Gomez <agomezdo18dw@ikzubirimanteo.com>
2019-09-20 15:53:55 +0200

    Acabado ejercicio funciones y pequeño cambio en permisos de algunos ejercicios bloque1

--- Primeros pasos/funciones.php
+++ Primeros pasos/funciones.php
@@ -9,6 +9,18 @@
         $n1 = rand(0,10);
         return $n1;
     }
+    function operacion(){
+        $n = random_int(1,4);
+        if ($n == 1)
+            $operacion = "suma";
+        elseif ($n == 2)
+            $operacion = "resta";
+        elseif ($n == 3)
+            $operacion = "mult";
+        else
+            $operacion = "div";
+        return $operacion;
+    }
     function suma ($n1,$n2){
         return $n1 + $n2;
     }
@@ -21,10 +33,24 @@
     function div ($n1,$n2){
         return $n1 / $n2;
     }   
+    function calculadora ($operacion, $n1, $n2){
+        if ($operacion == "suma")
+            $res = suma($n1, $n2);
+        elseif ($operacion == "resta")
+            $res = resta($n1, $n2);
+        elseif ($operacion == "mult")
+            $res = mult($n1, $n2);
+        else
+            $res = div($n1, $n2);
+        return $res;
+    }
     $n1 = numero();
     $n2 = numero();
-    echo "El primero numero es: $n1 <br>
-          El segundo numero es: $n2 "
+    $operacion = operacion();
+    echo "<p><b>El primero numero es:</b> ".$n1."<br>
+          <b>El segundo numero es:</b> ".$n2."<br>
+          <b>La operacion es:</b> ".$operacion."</p>";
+    echo "<p>La respuesta es: ".calculadora($operacion, $n1, $n2)."</p>";
 	?>
 </body>
 </html>
