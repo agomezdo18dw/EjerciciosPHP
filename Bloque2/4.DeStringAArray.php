4944d6afc6fc0fa9463182b8b6fdbebf9b0a968d
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-09-30 01:01:53 +0200

    Añado ejercicio4 pero sin empezar

---
+++ Bloque2/4.DeStringAArray.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio PHP/Bloque2/4.DeStringAArray</title>
+    <style>
+    </style>
+</head>
+<body>
+    <p>Dado un string formado por 5 palabras cualesquiera (por ejemplo $str = "manzana pera limón sandía melón"),
+    transformala en un array asociativo que tenga como índice la palabra y como valor la longitud de la misma.</p>
+	<?php
+    
+	?>
+</body>
+</html>
